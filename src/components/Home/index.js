1d8231cc352cb1c720419352f2b1d17ea0561cc4
Anisha Jain <anishajain@outlook.com>
2020-03-23 19:17:53 -0700

    art added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,40 +5,58 @@ import Button from "../Button";
 import ArtItem from "./components/ArtItem";
 import "./styles.css";
 
-import translation from "../../assets/art/translation.jpeg"
-import navratri from "../../assets/art/Navratri.jpg"
-import shackles from "../../assets/art/shackles.jpg"
+import translation from "../../assets/art/translation.jpeg";
+import navratri from "../../assets/art/Navratri.jpg";
+import shackles from "../../assets/art/shackles.jpg";
+import passion from "../../assets/art/Passion.jpg";
+import loss from "../../assets/art/Loss.jpg";
 
 const Home = ({ history }) => {
   const art = [
     {
       img: translation,
-      price: "$100",
+      price: "$300",
       title: "Translation"
     },
     {
-        img: navratri,
-        price: "$250",
-        title: "Navratri"
-      },
-      {
-        img: shackles,
-        price: "$350",
-        title: "Shackles"
-      },
+      img: passion,
+      price: "$300",
+      title: "Passion"
+    },
+    {
+      img: loss,
+      price: "$500",
+      title: "Loss"
+    },
+    {
+      img: shackles,
+      price: "$350",
+      title: "Shackles"
+    },
+    {
+      img: navratri,
+      price: "$250",
+      title: "Navratri"
+    }
   ];
 
   return (
     <div className="home">
-      <div className="tbd">
-      </div>
+      <div className="tbd"></div>
       <div className="inline">
         <Button isBig={true}>LEARN MORE</Button>
-        <Button isBig={true} onClick={()=> history.push('/contact')}>CONTACT</Button>
+        <Button isBig={true} onClick={() => history.push("/contact")}>
+          CONTACT
+        </Button>
       </div>
       <div className="art">
         {art.map(({ img, price, title }) => (
-          <ArtItem key={title} image={img} price={price} title={title}></ArtItem>
+          <ArtItem
+            key={title}
+            image={img}
+            price={price}
+            title={title}
+          ></ArtItem>
         ))}
       </div>
     </div>
