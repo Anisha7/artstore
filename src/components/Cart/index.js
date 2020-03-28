d119266d65141dc2f3ec526418767dba4f31babd
Anisha Jain <anishajain@outlook.com>
2020-03-27 22:37:23 -0700

    remove works

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import { artStoreData } from "../../data";
 import { removeItem, getItems } from '../../helpers/storage'
 
 import "./styles.css";
 
-const CartItem = ({ id }) => {
+const CartItem = ({ id, rerender }) => {
   const item = artStoreData.filter(item => item.id === id)[0];
   return (
     <div className="cartItem">
@@ -30,6 +30,7 @@ const CartItem = ({ id }) => {
             isSmall={true}
             onClick={() => {
                 removeItem(id)
+                rerender()
                 }}
           >
             REMOVE
@@ -40,12 +41,13 @@ const CartItem = ({ id }) => {
   );
 };
 const Cart = () => {
+  let  [,setState]=useState();
   const items = getItems()
   return (
     <div className="cart">
       <div>
         <h2>Your cart</h2>
-        {items ? items.map(id => <CartItem id={id} key={id} />) : null}
+        {items ? items.map(id => <CartItem id={id} key={id} rerender={() => setState({})} />) : null}
       </div>
       <h2>Submit interest form</h2>
     </div>
